03b2460d73e2d141152545f686f823e1d891b31e
Switch2Switch <switch2switchreal@gmail.com>
2018-06-20 16:38:25 -0500

    Add project files.

---
+++ Samsung Toolbox/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub Version_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim proc As New System.Diagnostics.Process()
+        proc = Process.Start("adbrecovery.bat", "")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim proc2 As New System.Diagnostics.Process()
+        proc2 = Process.Start("adbreboot.bat", "")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim proc5 As New System.Diagnostics.Process()
+        proc5 = Process.Start("killadb.bat", "")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim proc3 As New System.Diagnostics.Process()
+        proc3 = Process.Start("adbrestart.bat", "")
+    End Sub
+End Class
